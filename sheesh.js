b5941484c7ea68f9839ea7d45037c820ecfdd4b9
M307users <m307user@m307.uc-bcf.edu.ph>
2024-02-15 18:26:27 +0800

    Excercise_5

---
+++ sheesh.js
@@ -0,0 +1,11 @@
+var x= document.getElementById('myBtn');
+x.addEventListener("click", myFunction);
+x.addEventListener("click", someOtherFunction)
+
+function myFunction() {
+    alert ("Welcome Retsel Nanognat");
+}
+
+function someOtherFunction() {
+    alert ("This function was also executed");
+}
